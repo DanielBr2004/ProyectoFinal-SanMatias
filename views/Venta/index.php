373b5c9c00e08d8a4dd09bfa9ac01a86bf272e03
DanielBr2004 <bulejeroj@s1>
2024-10-07 23:04:38 -0500

    proceso venta 10 %

--- views/Venta/index.php
+++ views/Venta/index.php
@@ -11,29 +11,111 @@
                 </div>
                 <div class="card-body">
                     <form action="" id="form-kardex-huevos" autocomplete="off">
-                        <div class="row g-2 m-b2">
+                        <div class="row g-2">
                             <div class="col-md">
                                 <div class="form-floating">
-                                    <input type="number" class="form-control" id="" >
-                                    <label for="">Filtrar cliente</label>
+                                    <input type="number" class="form-control" id="nrodocumento" required>
+                                    <label for="nrodocumento">Numero de Documento</label>
                                 </div>
                             </div>
-                            <div class="col-md">
+
+                            <div class="col-md-9 mb-3">
                                 <div class="form-floating">
-                                    <input type="number" class="form-control" id="" disabled>
-                                    <label for="">Nro de Documento</label>
-                                </div>
-                            </div>
-                            <div class="col-md">
-                                <div class="form-floating">
-                                    <input type="number" class="form-control" id="" disabled>
-                                    <label for="">Stock Actual</label>
+                                    <input type="text" class="form-control" id="nombres" required>
+                                    <label for="nombres">Nombre </label>
                                 </div>
                             </div>
 
                         </div>
-                    </form>
+
+                        <div class="row g-2">
+
+                            <div class="col-md-3 mb-3">
+                                <div class="input-group">
+                                <div class="form-floating">
+                                    <select name="tipodocumento" id="tipodocumento" class="form-select" required >
+                                        <option value="">Seleccione</option>
+                                        <option value="1">RUC</option>
+                                        <option value="2">DNI</option>
+                                        <option value="3">CARNET EXTRANGERIA</option>
+                                    </select>
+                                    <label for="tipodocumento">Tipo de Documento</label>
+                                </div>
+                            </div>
+                            </div>
+
+                            <div class="col-md-9 mb-3">
+                                <div class="form-floating">
+                                    <input type="number" id="telefono" maxlength="150" class="form-control" required>
+                                    <label for="telefono">Direccion Fiscal</label>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="row g-2">
+                            <div class="col-md-3 mb-3">
+                                <div class="form-floating">
+                                    <input type="number" id="telefono" maxlength="150" class="form-control">
+                                    <label for="telefono">Email</label>
+                                </div>
+                            </div>
+
+                            <div class="col-md-3 mb-3">
+                                <div class="form-floating">
+                                    <input type="number" id="telefono" maxlength="150" class="form-control">
+                                    <label for="telefono">Telefono</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-header">
+                            Detalle de Venta
+                        </div>
+                        <div class="card-body">
+                        <div class="col-md">
+                    <div class="input-group">
+                    <button class="input-group-text btn btn-success" type="button" id="buscar-nrodocumento"><i class="bi bi-plus-lg"></i></button>
+                        <div class="form-floating">
+                        <input type="tel" class="form-control" maxlength="12" minlength="8"  id="nrodocumento" autofocus required>
+                        <label for="nrodocumento">Buscar Producto</label>
+                        </div>
+                        <button class="input-group-text btn btn-secondary" type="button" id="buscar-nrodocumento"><i class="bi bi-search"></i></button>
+                    </div>
+
+                    <div class="card-body">
+                    <table class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <tr>
+                            <th scope="col">Stock</th>
+                            <th scope="col">ID</th>
+                            <th scope="col">Producto</th>
+                            <th scope="col">Cantidad</th>
+                            <th scope="col">Precio Unitario</th>
+                            <th scope="col">Total</th>
+                            <th scope="col">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        
+                        </tbody>
+                        </table>
+                    </div>
+                </div> 
+                        </div>
+
+
+                
                 </div>
+
+                <div class="card-footer">
+                    <div class="text-end">
+                        <button type="submit" id="registrar-colaborador" class="btn btn-primary btn-sm">Registrar Venta</button>
+                        <button type="reset" id="cancelar" class="btn btn-secondary btn-sm">Cancelar Venta</button>
+                    </div>
+                </div>
+                    </form>
+
+                
             </div>
 
         </div>
