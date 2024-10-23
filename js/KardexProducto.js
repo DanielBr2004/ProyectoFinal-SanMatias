d229983d6ae77eaef2d36a7c6bf145209f6c5d45
DanielBr2004 <bulejeroj@s1>
2024-10-22 20:30:40 -0500

    solucionando datatable

--- js/KardexProducto.js
+++ js/KardexProducto.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   
   $('#tabla-productos').DataTable({
     "language": {
-        "url": "../views/js/Spanish.json"
+        "url": "//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json"
     }
 });
 
