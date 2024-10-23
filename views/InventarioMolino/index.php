5d3ec90929a974255c97e12ffcf42d1e3c39c3fb
Emersson25 <ortizjacobo25@gmail.com>
2024-10-23 18:42:51 -0500

    listadoKardexProducto50%

--- views/InventarioMolino/index.php
+++ views/InventarioMolino/index.php
@@ -184,7 +184,7 @@
     ></script>
     <!-- Bootstrap-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="<?= $host ?>/js/script1.js"></script>
+    <script src="<?= $host ?>/js/listkardexProd.js"></script>
 
 
         <!-- JS del Proyecto-->
