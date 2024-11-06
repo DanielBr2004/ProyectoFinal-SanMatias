a95105289f8000232d4203ad950093e80989e53a
Emersson25 <ortizjacobo25@gmail.com>
2024-11-06 08:34:27 -0500

    clientes spu eliminar

--- db/6. SPU-Clientes.sql
+++ db/6. SPU-Clientes.sql
@@ -60,3 +60,66 @@ BEGIN
         ON PER.idpersona = CLI.idpersona ORDER BY idcliente DESC;
 END $$
 call  spu_listar_cliente();
+-- -------------------------------- editar ----------------------------------
+DROP PROCEDURE IF EXISTS spu_cliente_editar;
+DELIMITER $$
+
+CREATE PROCEDURE spu_cliente_editar
+(
+    IN _idcliente INT,
+    IN _nrodocumento CHAR(12),
+    IN _tipodocumento CHAR(3),
+    IN _cliente_nombre VARCHAR(90)
+)
+BEGIN
+    DECLARE _idpersona INT;
+
+    -- Obtener el idpersona asociado con el idcliente proporcionado
+    SELECT idpersona INTO _idpersona
+    FROM cliente
+    WHERE idcliente = _idcliente;
+
+    -- Actualizar el campo `tipodocumento` en la tabla `cliente`
+    UPDATE cliente
+    SET 
+        tipodocumento = _tipodocumento
+    WHERE idcliente = _idcliente;
+
+    -- Actualizar el campo `nrodocumento` en la tabla `personas`
+    UPDATE personas
+    SET 
+        nrodocumento = _nrodocumento
+    WHERE idpersona = _idpersona;
+
+    -- Actualizar el nombre o razón social en función del `tipodocumento`
+    IF _tipodocumento = 'DNI' THEN
+        -- Si es DNI, actualizar el nombre completo en la tabla `personas`
+        UPDATE personas
+        SET 
+            nombres = _cliente_nombre,
+            apepaterno = '',   -- Aquí puedes agregar lógica si tienes el apellido paterno
+            apematerno = ''    -- Aquí puedes agregar lógica si tienes el apellido materno
+        WHERE idpersona = _idpersona;
+    ELSEIF _tipodocumento = 'RUC' THEN
+        -- Si es RUC, actualizar la razón social en la tabla `cliente`
+        UPDATE cliente
+        SET 
+            razonsocial = _cliente_nombre
+        WHERE idcliente = _idcliente;
+    END IF;
+END $$
+CALL spu_cliente_editar(3, '12345678', 'DNI', 'Juan Perez');
+-- ------------------------ ELIMINAR ----------------------------------
+DROP PROCEDURE IF EXISTS spu_cliente_eliminar;
+DELIMITER $$
+
+CREATE PROCEDURE spu_cliente_eliminar
+(
+    IN _idcliente INT
+)
+BEGIN
+    -- Eliminar el cliente de la tabla `cliente`
+    DELETE FROM cliente
+    WHERE idcliente = _idcliente;
+END $$
+CALL spu_cliente_eliminar(3);
