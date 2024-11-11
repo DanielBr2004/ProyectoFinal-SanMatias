af72cc29eff0741de28e281949cb44e7b48fe643
DanielBr2004 <bulejeroj@s1>
2024-11-11 01:43:18 -0500

    bajando los registros

--- db/DB_inserts/6. ISR-KardexHuevos.sql
+++ db/DB_inserts/6. ISR-KardexHuevos.sql
@@ -2,10 +2,10 @@ USE granjasanmatias;
 
 CALL GenerateKardexAlmHuevolote1();
 call GenerateKardexAlmHuevolote2();
-call GenerateKardexAlmHuevoUpdatedLote3();
-call GenerateKardexAlmHuevoLote4();
-call GenerateKardexAlmHuevoLote5();
-call GenerateKardexAlmHuevoLote6();
-call GenerateKardexAlmHuevoLote7();
-call GenerateKardexAlmHuevoLote8();
-call GenerateKardexAlmHuevoLote9();
+-- call GenerateKardexAlmHuevoUpdatedLote3();
+-- call GenerateKardexAlmHuevoLote4();
+-- call GenerateKardexAlmHuevoLote5();
+-- call GenerateKardexAlmHuevoLote6();
+-- call GenerateKardexAlmHuevoLote7();
+-- call GenerateKardexAlmHuevoLote8();
+-- call GenerateKardexAlmHuevoLote9();
