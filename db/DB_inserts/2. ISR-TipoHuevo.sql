eb3b6ce69bf8e897d473e267f38f08034be10be4
Emersson25 <ortizjacobo25@gmail.com>
2024-12-03 05:39:00 -0500

    inserthuevo

--- db/DB_inserts/2. ISR-TipoHuevo.sql
+++ db/DB_inserts/2. ISR-TipoHuevo.sql
@@ -1,13 +1,12 @@
 USE granjasanmatias;
 
-INSERT INTO tipoHuevo (tiposHuevos, PrecioKg, descripcion)
-VALUES
-('comercial', 1.50, 'Huevo comercial estándar'),
-('pardo', 1.75, 'Huevo de color pardo'),
-('sucio', 1.20, 'Huevo con suciedad'),
-('doble yema', 2.00, 'Huevo con doble yema'),
-('margarito', 1.80, 'Huevo tipo margarito'),
-('merma', 0.90, 'Huevo de merma');
+CALL spu_insertar_tipoHuevo('comercial', 1.50, 'Huevo comercial estándar');
+CALL spu_insertar_tipoHuevo('pardo', 1.75, 'Huevo de color pardo');
+CALL spu_insertar_tipoHuevo('sucio', 1.20, 'Huevo con suciedad');
+CALL spu_insertar_tipoHuevo('doble yema', 2.00, 'Huevo con doble yema');
+CALL spu_insertar_tipoHuevo('margarito', 1.80, 'Huevo tipo margarito');
+CALL spu_insertar_tipoHuevo('merma', 0.90, 'Huevo de merma');
+
 
 CALL spu_insertar_tipoHuevo('Huevo Bueno', 5.79, 'Huevo producido de manera orgánica');
 call spu_listado_tipohuevo();
